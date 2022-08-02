fd19d30e8e60c8801b206738b1f2051ef6eea489
M4TH3US17 <matheusdalvino50@gmail.com>
2022-08-02 16:11:54 -0400

    Enums (Cor & Tipo)

---
+++ src/main/java/com/example/demo/entities/enums/Tipo.java
@@ -0,0 +1,18 @@
+package com.example.demo.entities.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Tipo {
+
+    CONVERTIBLE("Convertible"), COUPE("Coupe"),
+    HATCHBACK("Hatchback"),     PICKUP("Pickup"),
+    SEDAN("Sedan"),             SUV("Suv"),
+    VAN("Van");
+
+    private String tipo;
+
+    Tipo(String tipo) { this.tipo = tipo; }
+
+    @JsonValue
+    public String getTipo() {return tipo;}
+}
