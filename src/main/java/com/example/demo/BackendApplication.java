40d2fcc0828054a86d48c1e848b5392e485d7d76
M4TH3US17 <matheusdalvino50@gmail.com>
2022-08-03 19:00:39 -0400

    Liberação do CORS

--- src/main/java/com/example/demo/BackendApplication.java
+++ src/main/java/com/example/demo/BackendApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*" /*http://localhost:3000*/)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
 }
