034a61ae186f8f6343e9354349401de90dddf35c
M4TH3US17 <matheusdalvino50@gmail.com>
2022-08-04 03:42:12 -0400

    Tratamento de Erro - 400.

--- src/main/java/com/example/demo/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/ApiExceptionHandler.java
@@ -2,10 +2,14 @@ package com.example.demo.exceptions;
 
 import com.example.demo.exceptions.notfound.*;
 import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -18,4 +22,17 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    protected ResponseEntity<Map<String, ErrorDetails>> validacaoCamposException(MethodArgumentNotValidException ex) {
+        Map<String, ErrorDetails> map = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldError = ((FieldError) error).getField();
+            ErrorDetails err = new ErrorDetails();
+            err.setError(error.getDefaultMessage());
+            err.setTimestamp(LocalDateTime.now().format(formatter));
+            err.setStatus(HttpStatus.BAD_REQUEST.value());
+            map.put(fieldError, err);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
+    }
 }
